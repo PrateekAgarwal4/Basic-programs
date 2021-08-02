4b1fcc721653fac1699eefb77107d8abc64b2787
PRATEEK AGARWAL <59892785+PrateekAgarwal4@users.noreply.github.com>
2021-08-02 23:00:07 +0530

    Add files via upload

---
+++ HCF.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int gcd(int a,int b)
+{
+	{
+	if(a==0 || b==0)
+	return -1;
+    }
+	if(a==b)
+	{
+		return a;
+	}
+	if(a>b)
+	{
+		return gcd(a-b,b);
+	}
+	else
+	{
+		return gcd(a,b-a);
+	}
+}
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	int c=gcd(a,b);
+	cout<<c;
+}
